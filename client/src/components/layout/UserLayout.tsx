1981dab27277e56b3d3d4d737ee12ec5e7643b0a
Peach-mac <118442144+model-map@users.noreply.github.com>
2025-11-10 17:33:04 +0530

    Footer - v0.0.1, Dummy setup

--- client/src/components/layout/UserLayout.tsx
+++ client/src/components/layout/UserLayout.tsx
@@ -1,14 +1,12 @@
-import Header from "../common/Header";
+import { Footer } from "@/components/common/Footer";
+import Header from "@/components/common/Header";
 
 const UserLayout = () => {
   return (
-    <div>
-      {/* 
-    Header
-    Main
-    Footer
-    */}
+    <div className="min-h-screen flex flex-col">
       <Header />
+      <main className="flex-1">{/* MAIN CONTENT HERE */}</main>
+      <Footer />
     </div>
   );
 };
